0b0fa16830e2e3cf199fe31d2d5a192ec3f81823
takanakahiko <takanakahiko@gmail.com>
2020-03-23 19:24:57 +0900

    add sample code for getting dfn informations

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 
-const Home = () => {
+import AWS from 'aws-sdk';
+import CloudFormationGraph from 'cloudformation-graph'
+
+const Home = (props) => {
   return (
     <React.Fragment>
       <Head>
@@ -20,5 +23,31 @@ const Home = () => {
     </React.Fragment>
   );
 };
+
+Home.getInitialProps = async () => {
+
+  const cloudformation = new AWS.CloudFormation({
+    accessKeyId: process.env.aws_access_key_id,
+    secretAccessKey: process.env.aws_secret_access_key,
+    region: 'ap-northeast-1'
+  });
+
+  // get StackResources
+  const resources = await cloudformation.listStackResources({
+    StackName: 'nakayama-cfn-visualize-test02', /* required */
+  }).promise()
+  console.log(resources.$response)
+
+  // get Template
+  const template = await cloudformation.getTemplate({
+    StackName: 'nakayama-cfn-visualize-test02'
+  }).promise();
+  const cfg = new CloudFormationGraph();
+  const ret = cfg.graph(template.TemplateBody)
+  console.log(ret)
+
+  return {}
+
+}
 
 export default Home;
